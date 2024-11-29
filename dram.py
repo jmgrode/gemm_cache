d50b27c4bc4c1cc0ddee1044123f9969b0f2c6d9
Sreya Gogineni <gosreya@gmail.edu>
2024-11-29 15:22:58 -0500

    changed dram

--- dram.py
+++ dram.py
@@ -1,38 +1,42 @@
 from packet import Packet
 from memory import MemObject
 from memory_array import MemoryArray
-import sys
+import math
 
 class Dram(MemObject):
     def __init__(self, size: int, read_latency: int, write_latency: int, burst_size: int = 64) -> None:
         super().__init__(size, size, read_latency, write_latency)
+        assert burst_size > 0, "Burst size must be greater than 0"
+
         self.memory = MemoryArray(size)
+        self.mem_size = size
         self.read_latency = read_latency
         self.write_latency = write_latency
         self.burst_size = burst_size
 
     def process_packet(self, pkt: Packet) -> Packet:
-        if (pkt.addr < 0) or (pkt.addr + pkt.size > len(self.memory)):
-            sys.exit("Error: out of bounds load or store to DRAM")
+        assert pkt.size > 0, "Error: Packet size must be greater than 0 for DRAM load/store"
+        assert (pkt.addr >= 0) and (pkt.addr + pkt.size <= self.mem_size), "Error: out of bounds load or store to DRAM"
 
         if (pkt.load):
             return self.retrieve(pkt)
         return self.store(pkt)
 
     def retrieve(self, pkt: Packet) -> Packet:
-        data = self.memory[pkt.addr : pkt.addr + pkt.size]
-        latency = (pkt.size // self.burst_size) * self.read_latency + pkt.latency
-        return Packet(True, pkt.addr, pkt.size, data, latency)
+        pkt.data = self.memory[pkt.addr : pkt.addr + pkt.size]
+        pkt.latency += (pkt.size // self.burst_size) * self.read_latency
+        return pkt
 
     def store(self, pkt: Packet) -> Packet:
+        assert math.ceil(pkt.data.bit_length() / 8) == pkt.size, "Error: Data size mismatch with packet size during DRAM load/store"
         self.memory[pkt.addr : pkt.addr + pkt.size] = pkt.data
-        latency = (pkt.size // self.burst_size) * self.write_latency + pkt.latency
-        return Packet(False, pkt.addr, 0, None, latency)
+        pkt.latency += (pkt.size // self.burst_size) * self.write_latency
+        return pkt
 
 if __name__ == "__main__":
     print("Testing DRAM")
 
-    DRAM_obj = Dram(size = 2048, read = 10, write = 12)
+    DRAM_obj = Dram(size=2048, read_latency=10, write_latency=12)
 
     # test write
     num_bytes = 256
@@ -41,8 +45,8 @@ if __name__ == "__main__":
     pkt_write_ret = DRAM_obj.process_packet(pkt_write)
     assert(pkt_write_ret.load == False)
     assert(pkt_write_ret.addr == 7)
-    assert(pkt_write_ret.size == 0)
-    assert(pkt_write_ret.data == None)
+    assert(pkt_write_ret.size == num_bytes)
+    assert(pkt_write_ret.data == data)
     assert(pkt_write_ret.latency == (256 / 64) * 12)
 
     # test read
@@ -54,20 +58,22 @@ if __name__ == "__main__":
     assert(pkt_read_ret.data == data)
     assert(pkt_read_ret.latency == (256 / 64) * 12 + (256 / 64) * 10)   
 
+    # test out of bounds
+    pkt_oob = Packet(load=True, addr=2040, size=num_bytes, data=data, latency = pkt_read_ret.latency)
+    try:
+        DRAM_obj.process_packet(pkt_oob)
+    except AssertionError as e:
+        pass
+    else:
+        assert(False)
+
     # test another read
-    expected_data = data & (data - 1)
+    expected_data = data << 8
     pkt_read = Packet(load=True, addr=8, size=num_bytes, data=None, latency = pkt_read_ret.latency)
     pkt_read_ret = DRAM_obj.process_packet(pkt_read)
     assert(pkt_read_ret.data == expected_data)
 
-    # test out of bounds
-    pkt_oob = Packet(load=True, addr=2040, size=num_bytes, data=data, latency = pkt_read_ret.latency)
-    try:
-        dram_obg.process_packet(pkt_oob)
-    except SystemExit as e:
-        pass
-    else:
-        assert(False)
+    print("Finished Testing DRAM")
 
 
 
