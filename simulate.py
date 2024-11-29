d736d465360963a3e3b8a542c0a490503b811797
Brandon Zhang <bdwzhang@umich.edu>
2024-11-29 15:02:06 -0500

    Started simulate.py

--- simulate.py
+++ simulate.py
@@ -2,6 +2,17 @@ from cpu import Cpu
 from dram import Dram
 from gemm_cache import GemmCache, Cache
 
-# TODO: determine how instructions should be put in
 # TODO: determine how to time everything
 
+class Simulator:
+    def __init__(self, cpu: Cpu) -> None:
+        self.cpu = Cpu
+        self.time = 0
+
+    def reset(self):
+        pass
+		# reset after a simulation
+
+    def simulate(self, instructions):
+        pass
+        #TODO: figure out what type instructions should be, probably a list of tuples which contain the arguments for instruction functions
